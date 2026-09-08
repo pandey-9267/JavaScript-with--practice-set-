8c93de079014e0840cac73b1d00a78409ee923f7
Abhishek Pandey <abhishekpandey9267@gmail.com>
2026-09-08 15:22:58 +0530

    learn the how the private works in the encapsulation

--- oop/encapsulation.js
+++ oop/encapsulation.js
@@ -1,3 +1,5 @@
+// it a public
+
 class BankAccount {
   constructor(balance) {
     this.balance = balance;
@@ -12,3 +14,32 @@ class BankAccount {
 const account = new BankAccount(1000);
 account.deposit(500);
 console.log(account.getbalance());
+
+// it a private  with use of "#"
+
+class BankAcc {
+  #balance;
+  constructor(balance) {
+    this.#balance = balance;
+  }
+  deposite(amount) {
+    if (amount > 0) {
+      this.#balance += amount;
+    }
+  }
+  withdraw(amount) {
+    if (amount > 0 && amount <= this.#balance) {
+      this.#balance -= amount;
+    }
+  }
+  getbalance() {
+    return this.#balance;
+  }
+}
+
+const acc = new BankAcc(1000);
+
+acc.deposite(5000);
+acc.withdraw(2000);
+
+console.log(acc.getbalance());
